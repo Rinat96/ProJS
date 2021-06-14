831cb64827fb7bfa21a27a59402ce4919ef8980b
Artem <carbon0093@gmail.com>
2021-06-14 12:01:49 +0300

    HW#5

---
+++ lesson5/Magazin/main.js
@@ -0,0 +1,37 @@
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    catalogUrl: '/catalogData.json',
+    products: [],
+    imgCatalog: 'https://via.placeholder.com/200x150'
+  },
+  methods: {
+
+    getJson(url) {
+      return fetch(url)
+        .then(result => result.json())
+        .catch(error => {
+          console.log(error);
+        })
+    },
+
+    addProduct(product) {
+      console.log(product.id_product);
+    }
+  },
+
+  mounted() {
+    this.getJson(`${API + this.catalogUrl}`)
+      .then(data => {
+        for (let el of data) {
+          this.products.push(el);
+        }
+      });
+  }
+});
